cb5e74ae7264b73d41dcc954a60f6db2a2bcd31b
fmindrayana2 <fmindrayana2@gmail.com>
2022-07-17 13:30:10 +0700

    13:30:10.24-2022-07-17

--- sinkron github.bat
+++ sinkron github.bat
@@ -1,6 +1,6 @@
 echo off
 set datestr=%date:~10,4%-%date:~4,2%-%date:~7,2%
 git add .
-git commit -m "time /t"
+git commit -m "%time%-%datestr%"
 git push origin master
 title alhamdulillah selesai..
